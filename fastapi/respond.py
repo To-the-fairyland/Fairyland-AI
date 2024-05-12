da57e3edfe2a5652bf0de9f586b8c352e13fbf1f
[dongje] <[foryui25@naver.com]>
2024-05-12 13:47:02 +0900

    fastapi2

--- fastapi/respond.py
+++ fastapi/respond.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 from typing import List
 import numpy as np
 import librosa
@@ -23,12 +23,6 @@ async def predict_emotion_endpoint(audiofile: UploadFile = File(...)):
     with open(filename, "wb") as buffer:
         shutil.copyfileobj(file, buffer)
 
-    # Get the current directory
-    current_directory = os.getcwd()
-    # List all files in the current directory
-    files = os.listdir(current_directory)
-    print(files)
-
     try:
         emotion = speech_recog.predict(filename)
     except FileNotFoundError:
@@ -36,10 +30,23 @@ async def predict_emotion_endpoint(audiofile: UploadFile = File(...)):
 
     return {"emotion": emotion}
 
-@app.post("/asr-similarity/")
-async def asr_similarity(file_directory: str,GT : str):
+class similarity_audiofile(BaseModel):
+    file : UploadFile
+    groundtruth : str
 
-    transcript = similar_check.asr(file_directory)
+@app.post("/asr-similarity/")
+async def asr_similarity(audio_file: UploadFile = UploadFile(...), groundtruth: str = Form(...)):
+
+    filename = 'imsi.wav'
+
+    print(audio_file)
+    print(groundtruth)
+
+    file,GT = audio_file.file , groundtruth
+    with open(filename, "wb") as buffer:
+        shutil.copyfileobj(file, buffer)
+
+    transcript = similar_check.asr(filename)
     similarity = similar_check.similar_check(transcript,GT)
 
     return {"transcript": transcript , "similarity" : similarity}
